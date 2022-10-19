cedcdbe45a98607255249c90489c44ae415f2fb1
Julia Barbosa <C0627651@globalvale.onmicrosoft.com>
2022-10-18 21:44:20 -0300

    exemplos dos matchers

---
+++ src/app/teste/teste.component.spec.ts
@@ -0,0 +1,105 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TesteComponent } from './teste.component';
+
+describe('TesteComponent', () => { //suite
+  let component: TesteComponent;
+  let fixture: ComponentFixture<TesteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TesteComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TesteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('deve criar o componente test', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('validar o matcher toEqual', () => {
+    const aluno = {value: true};
+    const professor = {value: true};
+
+    expect(aluno).toEqual(professor);
+
+  });
+
+  it('validar o matcher toEqual', () => {
+    const aluno = {value: true};
+    const professor = {value: true};
+
+    expect(aluno).toBe(aluno); //vai passar
+    //expect(aluno).toBe(professor); //vai falhar
+  });
+
+  it('validar o value quando o metodo for chamado', () => {
+    //const value = 'aluno'
+    expect(component.transform('0')).toBe('0');
+  });
+
+  it('validar o cep', () => {
+    expect("12345-678").toMatch(/^\d{5}-\d{3}$/);
+  });
+
+  it('should be defined', () => {
+    let bandaBTS = 'oi';
+
+    expect(bandaBTS).toBeDefined();
+  });
+
+  it('should be null', () => {
+    let dinheiro = null;
+
+    expect(dinheiro).toBeNull();
+  });
+
+  it('should be null', () => {
+    let value = null;
+    expect(component.transformNull(value)).toBeNull();
+  });
+
+  it('should be true', () => {
+    let value = true;
+    expect(value).toBeTruthy();
+  });
+
+  it('should be maior que 7', () => {
+    expect(component.testTruthy(3)).not.toBeTruthy();
+  });
+
+  it('should be array fruits', () => {
+   let frutas = ["maçã", "laranja", "banana", "uva"];
+
+   expect(frutas).toContain("maçã");
+  });
+
+  it('should be Apple', () => {
+    expect(component.shouldBeApple()).toContain('maçã');
+    expect(component.shouldBeApple()).toContain('laranja');
+    expect(component.shouldBeApple()).toContain('banana');
+  });
+
+  it('should be maior que 20', () => {
+    expect(component.shouldBeGreater()).toBeLessThan(10);
+  });
+
+  it('should be toThrow', () => {
+    expect(component.multiplicar).toThrow();
+  });
+
+  it('should be toThrow', () => {
+    expect(component.multiplicar).toThrowError();
+  });
+
+  it('should be toThrow type', () => {
+    expect(function() {component.soma(0,0)}).toThrowError(TypeError);
+    expect(function() {component.soma(0,0)}).toThrowError(/maior que o/);
+  });
+
+
+});
---
+++ src/app/teste/teste.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  templateUrl: './teste.component.html',
+  styleUrls: ['./teste.component.css']
+})
+export class TesteComponent implements OnInit {
+  numero!: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  transform(value: string): string{
+    if(!value){
+      return '-';
+    }
+    return value;
+  }
+
+  transformNull(value: null): null{
+    if(!value){
+      return null;
+    }
+    return value;
+  }
+
+  testTruthy(qq: number){
+    if(qq > 5){
+      return true;
+    }
+    return false;
+  }
+
+  shouldBeApple(): string[] {
+    return ["maçã", "laranja", "banana"];
+  }
+
+  shouldBeGreater(): number {
+    return 1;
+  }
+
+  multiplicar(): number{
+    if (this.numero <= 0){
+      throw new TypeError("deve ser maior que o")
+    }
+    return this.numero * this.numero
+  }
+
+  soma(n1:number, n2:number):number{
+    if(n1 <=0 || n2 <= 0){
+      throw new TypeError("deve ser maior que o")
+    }
+    return n1+n2;
+  }
+
+
+
+
+
+
+}
